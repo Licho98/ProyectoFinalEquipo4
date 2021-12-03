9537f2710b77d325db844fa3abf0dfd4065a6d69
Licho98 <93675120+Licho98@users.noreply.github.com>
2021-12-03 16:25:01 -0600

    Add files via upload

---
+++ src/test/java/com/example/PruebaGoogle/Agregar.java
@@ -0,0 +1,150 @@
+package com.example.PruebaGoogle;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Agregar {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+  JavascriptExecutor js;
+  @Before
+  public void setUp() throws Exception {
+	  WebDriverManager.chromedriver().setup();
+    driver = new ChromeDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    js = (JavascriptExecutor) driver;
+  }
+
+  @Test
+  public void testAgregar() throws Exception {
+    driver.get("http://localhost:3000/");
+    driver.findElement(By.xpath("//div[@id='root']/div/div[2]/button")).click();
+    driver.findElement(By.name("name")).click();
+    driver.findElement(By.name("name")).clear();
+    driver.findElement(By.name("name")).sendKeys("Maria");
+    driver.findElement(By.name("email")).click();
+    driver.findElement(By.name("email")).clear();
+    driver.findElement(By.name("email")).sendKeys("Maria@gmail.com");
+    driver.findElement(By.name("age")).click();
+    driver.findElement(By.name("age")).clear();
+    driver.findElement(By.name("age")).sendKeys("22");
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Gender'])[2]/following::div[2]")).click();
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Female'])[1]/following::span[1]")).click();
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Woah!'])[1]/following::button[1]")).click();
+    driver.findElement(By.xpath("//i")).click();
+    driver.findElement(By.xpath("//div[@id='root']/div/div[2]/table/tbody/tr/td")).click();
+    pause(5000);
+    try {
+      assertEquals("Maria", driver.findElement(By.xpath("//div[@id='root']/div/div[2]/table/tbody/tr/td")).getText());
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+  }
+  
+  @Test
+  public void testModificar() throws Exception {
+    driver.get("http://localhost:3000/");
+    driver.findElement(By.xpath("//div[@id='root']/div/div[2]/table/tbody/tr/td[5]/button")).click();
+    driver.findElement(By.name("name")).click();
+    driver.findElement(By.name("name")).clear();
+    driver.findElement(By.name("name")).sendKeys("Maria Tapia");
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Email'])[2]/following::label[1]")).click();
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Woah!'])[1]/following::button[1]")).click();
+    driver.findElement(By.xpath("//i")).click();
+    driver.findElement(By.xpath("//div[@id='root']/div/div[2]/table/tbody/tr/td")).click();
+    
+    String textoEjecucion = driver.findElement(By.xpath("//div[@id='root']/div/div[2]/table/tbody/tr/td")).getText();
+    String textoEsperado = "Maria Tapia";
+    assertThat(textoEsperado, is(textoEjecucion));
+  }
+
+  @Test
+  public void testEliminar() throws Exception {
+    driver.get("http://localhost:3000/");
+    driver.findElement(By.xpath("//div[@id='root']/div/div[2]/table/tbody/tr/td[5]/button[2]")).click();
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Maria Tapia'])[2]/following::button[1]")).click();
+    driver.findElement(By.xpath("//div[@id='root']/div/div[2]/table/tbody/tr/td")).click();
+    String textoEjecucion = driver.findElement(By.xpath("//div[@id='root']/div/div[2]/table/tbody/tr/td")).getText();
+    String textoEsperado = "Maria Tapia";
+    assertThat(textoEsperado, not(textoEjecucion));
+    
+    /*try {
+      assertEquals("Pepe", driver.findElement(By.xpath("//div[@id='root']/div/div[2]/table/tbody/tr/td")).getText());
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+    driver.findElement(By.xpath("//div[@id='root']/div/div[2]/table/tbody/tr[2]/td")).click();
+    try {
+      assertEquals("Harold", driver.findElement(By.xpath("//div[@id='root']/div/div[2]/table/tbody/tr[2]/td")).getText());
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+    driver.findElement(By.xpath("//div[@id='root']/div/div[2]/table/tbody/tr[3]/td")).click();
+    try {
+      assertEquals("Luis", driver.findElement(By.xpath("//div[@id='root']/div/div[2]/table/tbody/tr[3]/td")).getText());
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }*/
+  }
+  
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+ }
+  private void pause(long mils) {
+	  try {
+		  Thread.sleep(mils);
+	  }catch(Exception e){
+		  e.printStackTrace();
+	  }
+  }
+}
